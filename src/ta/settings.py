3a2fd557ac5a24cdafd402ea534cdaba215817e6
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2013-09-05 09:19:54 +0200

    added multithread dev server

--- src/ta/settings.py
+++ src/ta/settings.py
@@ -130,6 +130,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'concurrent_server',
+
     'assistant',
     'taggit',
 
