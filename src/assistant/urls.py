23a216b43c0624f9f64961957e27aa03f7bab34d
Arun K. R. kra3 <the1.arun@gmail.com>
2013-09-12 00:04:32 +0200

    change in routing for adding a test

--- src/assistant/urls.py
+++ src/assistant/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'assistant.views.home'),
-    url(r'^test/add$', 'assistant.views.test_add'),
-    url(r'^test/edit/(\d+)$$', 'assistant.views.test_edit'),
+    url(r'^test/add$', 'assistant.views.test_edit', {'pk': None}),
+    url(r'^test/edit/(?P<pk>\d+)$', 'assistant.views.test_edit'),
     url(r'^test/detail/(\d+)$', 'assistant.views.test_detail'),
     url(r'^test/delete/(\d+)$', 'assistant.views.test_delete'),
     url(r'^test/filter$', 'assistant.views.test_filter'),
