73106dc436e212487628210db0d0fba7161072b8
Arun K. R. kra3 <the1.arun@gmail.com>
2013-09-22 00:38:48 +0200

    Fixing settings.py

--- assistant/settings.py
+++ assistant/settings.py
@@ -1,2 +1,25 @@
 import os
+from django.conf import settings
 
+WHOOSH_PATH = getattr(settings, "TEST_ASSISTANT_WHOOSH_PATH", '.')
+
+INSTALLED_APPS = settings.INSTALLED_APPS + (
+    'concurrent_server',
+
+    'taggit',
+    'taggit_machinetags',
+    'django_nvd3',
+    'haystack',
+
+    'assistant',
+)
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(WHOOSH_PATH, 'whoosh_index'),
+    },
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
