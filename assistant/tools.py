45a1736ff45aec5fcbec8d36dad66d85e7e714d7
Arun K. R. kra3 <the1.arun@gmail.com>
2013-09-22 01:00:24 +0200

    Fixed a bug in tools.py

--- assistant/tools.py
+++ assistant/tools.py
@@ -7,7 +7,7 @@ def instances_to_csv(instances):
     output = StringIO()
     writer = csv.writer(output)
     for instance in instances:
-        o = list)(
+        o = list()
         o.append(instance.run.version)
         o.append(instance.test.title)
         o.append(instance.test.description)
